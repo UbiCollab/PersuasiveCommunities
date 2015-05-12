29679a5c9b9c068b82e3a020556180ed61bbe9fc
thomfra <franangthomas@gmail.com>
2015-05-12 19:39:13 +0200

    Fixed

--- cossmicvagrant/data/emoncms/Modules/cossmiccontrol/Views/summary.php
+++ cossmicvagrant/data/emoncms/Modules/cossmiccontrol/Views/summary.php
@@ -4,11 +4,11 @@ $decomposedPath = explode("/", "$path");
 $vdPath = "";
 foreach($decomposedPath as &$value) {
     if((strcmp($value, "http:") == 0) || (strcmp($value, "https:") == 0)){
-		$vdPath .= $value . "//";
+        $vdPath .= $value . "//";
     }else{
-		if((empty($value) == false)  && (strcmp($value, "emoncms") !== 0)){
-			$vdPath .= $value . "/";
-		}
+        if((empty($value) == false)  && (strcmp($value, "emoncms") !== 0)){
+            $vdPath .= $value . "/";
+        }
     }
 }
 ?>
@@ -21,10 +21,9 @@ foreach($decomposedPath as &$value) {
 <link rel="stylesheet" type="text/css" href="<?php echo $path; ?>Modules/cossmiccontrol/Views/simpleweather-geolocation-js/css/style.css">
 <!-- Stylesheet for the bar charts -->
 <link rel="stylesheet" type="text/css" href="<?php echo $path; ?>Modules/cossmiccontrol/Views/BarChartJS/barstyle.css">
-<link rel="stylesheet" type="text/css" href="<?php echo $path; ?>Lib/jquerypowetip/css/jquery.powertip.css">
+<link rel="stylesheet" type="text/css" href="<?php echo $path; ?>Lib/jquerypowertip/css/jquery.powertip.css">
 <!-- Stylesheet used for the scheduled tasks list 
 <link rel="stylesheet" type="text/css" href="<?php echo $path; ?>Lib/pure-0.5.0/pure-min.css">-->
-
 
 <!-- The D3 library used for the bar charts -->
 <script type="text/javascript" src="<?php echo $path; ?>Lib/d3.v3.min.js"></script>
@@ -154,27 +153,27 @@ foreach($decomposedPath as &$value) {
 			</div>
 		</div>
     </div>
-	<div class="row">
-		<div id="summarySchedule" class="panel span12">
-			<div class="panel-heading">Your scheduled tasks</div>
-			<table class="table table-condensed" id="taskTable">
-				<thead>
-					<tr>
-						<th>Device Name</th>
-						<th>Status</th>
-						<th>Program</th>
-						<th>Earliest Start Time</th>
-						<th>Latest Start Time</th>
-						<th>Actual Start Time</th>
-						<th>Actual Finishing Time</th>
-						<th></th>
-					</tr>
-			  </thead>
-			  <tbody>
-			  </tbody>
-			</table>
-		</div>
-	</div>
+    <div class="row">
+        <div id="summarySchedule" class="panel span12">
+            <div class="panel-heading">Your scheduled tasks</div>
+            <table class="table table-condensed" id="taskTable">
+                <thead>
+                    <tr>
+                        <th>Device Name</th>
+                        <th>Status</th>
+                        <th>Program</th>
+                        <th>Earliest Start Time</th>
+                        <th>Latest Start Time</th>
+                        <th>Actual Start Time</th>
+                        <th>Actual Finishing Time</th>
+                        <th></th>
+                    </tr>
+              </thead>
+              <tbody>
+              </tbody>
+            </table>
+        </div>
+    </div>
 </div>
 
 <!-- Div containing the full width graph for the neighborhood -->
@@ -378,9 +377,9 @@ $userlocation = $row['location'];
 				$("#cossmictreeContainer").css({"width":"100%"});
 				$("#cossmictree").animate({"margin-left":"100px"});
                 $("#treeHelp").toggle();
-
-                setTimeout(function(){	
-                    $("#weatherbox").toggle(500);
+                
+                setTimeout(function(){
+					$("#weatherbox").toggle(500);
 					$("#housebox").toggle(500); 
                 }, 200);   
             }
@@ -725,101 +724,101 @@ $userlocation = $row['location'];
                 mouseOnToPopup:true
             });
     }
-	
-	//Functions for loading the scheduled tasks table
-	function scheduledTaskLoad(){
-		var listTaskJson = '?json={"status":10,"info":true}';
-		
-		$.when(
-			$.ajax({
-				url: '<?php echo $path; ?>mas/list.json' + listTaskJson,
-				type: 'get',
-				dataType: 'json'
-			})
-		   ,
-			$.ajax({
-				url: '<?php echo $vdPath; ?>virtualDevices/device.php',
-				type: 'get',
-				dataType: "json",
-				data: {'json':'{"cmd":"list","user":"1"}'}
-			})
-		)
-		.then(function( resultListTask,resultListDevices) {
-			console.log(JSON.stringify(resultListTask[0]));
-			console.log(JSON.stringify(resultListDevices[0]));
-			var deviceHash = new Object();
-			$.each(resultListDevices[0].devicelist, function(idx, item){
-				$("#summarySchedule").show();
-				deviceHash[item.id] = item.name;
-			});
-			$.each(resultListTask[0].tasks, function(idx, item){
-				var id = item.id;
-				var devId = item.deviceID;
-				var status;
-				switch(item.status) {
-					case "0":
-						status = "Not yet scheduled"
-						break;
-					case "1":
-						status = "Scheduled"
-						break;
-					case "4":
-						status = "Running"
-						break;
-					case "5":
-						status = "Completed"
-						break;
-					default:
-						status = "Undetermined"
-				}
-			   
-				var mode = item.mode;
-				var est = item.EST;
-				var lst = item.LST;
-				var aet = item.AET;
-				var ast = item.AST;
-				var name = '';
-				if (deviceHash.hasOwnProperty(devId)) {
-					console.log("has own property");
-					name = deviceHash[devId];
-				}
-				var htmlRow= '<tr task-id="' +id+ '" device-id="' + devId + '"><td>' + name + ' </td><td> ' + status + ' </td><td> ' + mode +  ' </td><td> ' + est + ' </td><td> ' + lst +
-				' </td><td> ' + ast + ' </td><td>' + aet + '</td><td><a href="#" onclick="deleteTask('+id+',\''+aet+'\')"><i class="icon-trash"></i></a></td></tr>';
-				$("#taskTable > tbody").prepend(htmlRow);        
-			});
-		});
-	}
 
-	function deleteTask(id,aet){
-		if (aet != "UNDEFINED"){
-			window.alert("It is not possible to delete scheduled tasks ");
-			return;
-		}
-		
-		$.ajax({
-			url: '<?php echo $path; ?>mas/delete.json?id=' + id,
-			type: 'get',
-			dataType: 'json',
-			success: function(output) {
-				console.log(output);
-				if(null != output) {
-					if (output.result != "success"){
-						if(!output.result){ // task deleted
-							window.alert("It was not possible to delete the task, it probably has already been scheduled");
-						}
-						location.reload();
-					}
-				}
-				 location.reload();
-			},
-			error: function(xhr, desc, err) {
-				console.log(xhr);
-				console.log('Details: ' + desc + '\nError:' + err);
-				window.alert("error when trying to delete the task");
-			}
-		}); // end ajax call
-	}
-	//End of functions for the scheduled task table
+    //Functions for loading the scheduled tasks table
+    function scheduledTaskLoad(){
+        var listTaskJson = '?json={"status":10,"info":true}';
+        
+        $.when(
+            $.ajax({
+                url: '<?php echo $path; ?>mas/list.json' + listTaskJson,
+                type: 'get',
+                dataType: 'json'
+            })
+           ,
+            $.ajax({
+                url: '<?php echo $vdPath; ?>virtualDevices/device.php',
+                type: 'get',
+                dataType: "json",
+                data: {'json':'{"cmd":"list","user":"1"}'}
+            })
+        )
+        .then(function( resultListTask,resultListDevices) {
+            console.log(JSON.stringify(resultListTask[0]));
+            console.log(JSON.stringify(resultListDevices[0]));
+            var deviceHash = new Object();
+            $.each(resultListDevices[0].devicelist, function(idx, item){
+                $("#summarySchedule").show();
+                deviceHash[item.id] = item.name;
+            });
+            $.each(resultListTask[0].tasks, function(idx, item){
+                var id = item.id;
+                var devId = item.deviceID;
+                var status;
+                switch(item.status) {
+                    case "0":
+                        status = "Not yet scheduled"
+                        break;
+                    case "1":
+                        status = "Scheduled"
+                        break;
+                    case "4":
+                        status = "Running"
+                        break;
+                    case "5":
+                        status = "Completed"
+                        break;
+                    default:
+                        status = "Undetermined"
+                }
+               
+                var mode = item.mode;
+                var est = item.EST;
+                var lst = item.LST;
+                var aet = item.AET;
+                var ast = item.AST;
+                var name = '';
+                if (deviceHash.hasOwnProperty(devId)) {
+                    console.log("has own property");
+                    name = deviceHash[devId];
+                }
+                var htmlRow= '<tr task-id="' +id+ '" device-id="' + devId + '"><td>' + name + ' </td><td> ' + status + ' </td><td> ' + mode +  ' </td><td> ' + est + ' </td><td> ' + lst +
+                ' </td><td> ' + ast + ' </td><td>' + aet + '</td><td><a href="#" onclick="deleteTask('+id+',\''+aet+'\')"><i class="icon-trash"></i></a></td></tr>';
+                $("#taskTable > tbody").prepend(htmlRow);        
+            });
+        });
+    }
+
+    function deleteTask(id,aet){
+        if (aet != "UNDEFINED"){
+            window.alert("It is not possible to delete scheduled tasks ");
+            return;
+        }
+        
+        $.ajax({
+            url: '<?php echo $path; ?>mas/delete.json?id=' + id,
+            type: 'get',
+            dataType: 'json',
+            success: function(output) {
+                console.log(output);
+                if(null != output) {
+                    if (output.result != "success"){
+                        if(!output.result){ // task deleted
+                            window.alert("It was not possible to delete the task, it probably has already been scheduled");
+                        }
+                        location.reload();
+                    }
+                }
+                 location.reload();
+            },
+            error: function(xhr, desc, err) {
+                console.log(xhr);
+                console.log('Details: ' + desc + '\nError:' + err);
+                window.alert("error when trying to delete the task");
+            }
+        }); // end ajax call
+    }
+    //End of functions for the scheduled task table
 </script>
 <script>
 /*
@@ -908,7 +907,7 @@ $(document).ready( function () {
 	summarySetup();
 	addDataValues();
     setTooltips();
-	scheduledTaskLoad();
+    scheduledTaskLoad();
 });
 
 function  summarySetup(){
